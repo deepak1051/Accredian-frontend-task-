d1f2156af270d7d37f5d71fcb822cccf0be3233a
deepak1051 <lightyagamigoku@gmail.com>
2023-12-11 19:11:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Register />} />
+      </Routes>
+
+    </BrowserRouter>
+
+  )
+}
+
+export default App
